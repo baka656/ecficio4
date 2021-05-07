404727e8393c035f95e85f85d527c84dcfcb6371
baka656 <abbhinav.nomulla656@gmail.com>
2021-05-07 23:22:26 +0530

    '07-05-2021'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { EventsRegisterComponent } from './events-register/events-register.component';
+import { Page404Component } from './page404/page404.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IttopService } from './ittop.service';
+import { WindowRefService } from './window-ref.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EventsHomeComponent } from './events-home/events-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    EventsRegisterComponent,
+    Page404Component,
+    EventsHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    IttopService,
+    WindowRefService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
